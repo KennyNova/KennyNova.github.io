c7f52ece28c52b71a7634c7363f4f231e7b53f65
NavidMadani <navidmadani4598@gmail.com>
2021-06-21 17:20:23 -0400

    added links to pages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,32 @@ import { useRef } from 'react';
 function App() {
   const page1 = useRef(null)
   const page2 = useRef(null)
+  const page3 = useRef(null)
+  const page4 = useRef(null)
 
-  const executeScroll = (p) => {
-    console.log(p);
-    page1.current.scrollIntoView({ block: 'end', behavior: 'smooth' })
+  function scroll(page){
+    page.current.scrollIntoView({ block: 'end', behavior: 'smooth' })
   }
 
   return (
-    <div className="main-window bg-gradient-to-b to-blue-800 from-purple-600">
-      <span className="fixed text-gray-200 justify-items-center" >
-        <a onClick={executeScroll(0)} className="justify-items-center">home</a>
-        <a onClick={executeScroll}>login</a>
-        <a onClick={executeScroll}>register</a>
+    <div className="main-window bg-gradient-to-b from-purple-600 to-blue-800 ">
+      <span className="fixed text-gray-200 pt-10 grid grid-flow-col auto-cols-auto justify-items-center w-full " >
+        <p className="link float-left" onClick={() => { scroll(page1) }}>about me</p>
+        <p className="link float-left" onClick={() => { scroll(page2) }}>projects</p>
+        <p className="link float-left" onClick={() => { scroll(page3) }}>contact</p>
+        <p className="link float-left" onClick={() => { scroll(page4) }}>other</p>
       </span>
       <ul>
-        <li ref={page1}>part1
+        <li ref={page1}>about me
             
         </li>
-        <li ref={page2}>part2
+        <li ref={page2}>projects
             
         </li>
-        <li>part3
+        <li ref={page3}>contact
+                
+        </li>
+        <li ref={page4}>other
                 
         </li>
       </ul>
