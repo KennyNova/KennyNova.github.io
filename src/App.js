e0f36292860e47dad1e78e70bef9ad80241c32e4
Navid Madani <navidmadani4598@gmail.com>
2021-06-21 15:57:36 -0400

    sticky scrolling effect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
+  const page1 = useRef(null)
+  const page2 = useRef(null)
+
+  const executeScroll = (p) => {
+    console.log(p);
+    page1.current.scrollIntoView({ block: 'end', behavior: 'smooth' })
+  }
+
   return (
-    <div>hi</div>
+    <div className="main-window bg-gradient-to-b to-blue-800 from-purple-600">
+      <span className="fixed text-gray-200 justify-items-center" >
+        <a onClick={executeScroll(0)} className="justify-items-center">home</a>
+        <a onClick={executeScroll}>login</a>
+        <a onClick={executeScroll}>register</a>
+      </span>
+      <ul>
+        <li ref={page1}>part1
+            
+        </li>
+        <li ref={page2}>part2
+            
+        </li>
+        <li>part3
+                
+        </li>
+      </ul>
+      </div>
   );
 }
 
