d7187baed03bf49d80d7237f95adbd25b976ba6b
Navid Madani <navidmadani4598@gmail.com>
2021-06-29 18:35:25 -0400

    started on other page

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,4 +1,5 @@
-import '../styles/stars.css'
+import '../styles/slideshow.css'
+import React, { useState } from 'react';
 import boat from '../images/slideshow/boat.jpg'
 import duck from '../images/slideshow/duck.jpg'
 import flowers from '../images/slideshow/flowers.jpg'
@@ -9,23 +10,29 @@ import vannyprofile from '../images/slideshow/vannyprofile.png'
 import wheat from '../images/slideshow/wheat.jpg'
 
 export const Slideshow = (props) => {
-    const allImages = [{boat}, {duck}, {flowers}, {garden}, {statue}, {vanny}, {vannyprofile}, {wheat}]
+    const allImages = [boat, duck, flowers, garden, statue, vanny, vannyprofile, wheat]
+    var [index, setIndex] = useState(0)
 
+    function changeImage(direction) {
+        if (direction === 'right') setIndex(index++)
+        if (direction === 'left') setIndex(index--)
+        if (index > 7) setIndex(0)
+        if (index < 0) setIndex(7)
+        console.log(direction)
+        console.log(index + allImages[index] + direction)
+    }
 
-
-
-    const images = () => {
-        
+    function slideshow() {
         return (
-            allImages.map(picture => {
-                <img src={picture}></img>
-            })
+            <img key={index} src={allImages[index]}></img>
         )
     }
 
     return (
         <div className="slideshow-wrapper">
-            {images}
+            <a className="button float-left" onClick={(e => {changeImage("left")})}>left</a>
+            {slideshow()}
+            <a className="button float-right" onClick={(e => {changeImage('right')})}>right</a>
         </div>
     )
 }
