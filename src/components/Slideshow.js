e9c193f76b714cb11245a4dd7670c9f8310fb082
Navid Madani <navidmadani4598@gmail.com>
2021-06-30 00:03:34 -0400

    left right movement. fade in out

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,5 +1,5 @@
 import '../styles/slideshow.css'
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import boat from '../images/slideshow/boat.jpg'
 import duck from '../images/slideshow/duck.jpg'
 import flowers from '../images/slideshow/flowers.jpg'
@@ -12,27 +12,23 @@ import wheat from '../images/slideshow/wheat.jpg'
 export const Slideshow = (props) => {
     const allImages = [boat, duck, flowers, garden, statue, vanny, vannyprofile, wheat]
     var [index, setIndex] = useState(0)
+    var [prevIndex, setPrevIndex] = useState(0)
+    const prevJsx = <img className="fade-out" key={prevIndex} src={allImages[prevIndex]}></img>
+    const jsx = <img className="fade-in" key={index} src={allImages[index]}></img>
 
-    function changeImage(direction) {
-        if (direction === 'right') setIndex(index++)
-        if (direction === 'left') setIndex(index--)
+    useEffect(() => {
         if (index > 7) setIndex(0)
         if (index < 0) setIndex(7)
-        console.log(direction)
-        console.log(index + allImages[index] + direction)
-    }
-
-    function slideshow() {
-        return (
-            <img key={index} src={allImages[index]}></img>
-        )
-    }
+    }, [index])
 
     return (
-        <div className="slideshow-wrapper">
-            <a className="button float-left" onClick={(e => {changeImage("left")})}>left</a>
-            {slideshow()}
-            <a className="button float-right" onClick={(e => {changeImage('right')})}>right</a>
+        <div className="main-div">
+            <a className="button float-left" onClick={() => { setPrevIndex(index); setIndex(index - 1) }}>left</a>
+            <div className="slideshow-wrapper">
+                {prevJsx}
+                {jsx}
+            </div>
+            <a className="button float-right" onClick={() => { setPrevIndex(index); setIndex(index + 1)}}>right</a>
         </div>
     )
 }
