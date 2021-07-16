024a0a5bf16dfe6110cfb6f7be8f42ae95d2002c
Navid Madani <navidmadani4598@gmail.com>
2021-07-15 22:51:35 -0400

    intro animation

--- src/module/split.js
+++ src/module/split.js
@@ -2,9 +2,14 @@ export const splitWord = (text) => {
     
     const newText = text.split(' ')
 
-
-
     return (
-        newText.map((word, index) => <h1 key={word} index={index}>{word}</h1>)
+        newText.map((word, index) => (
+            <h1 key={word}
+                className={(index % 2 == 1) ? "tes" : "tes highlight"}
+                style={{
+                    animationDuration: ((index + 2) * .75) + "s"
+                }}
+            >{word}</h1>
+        ))
     )
 }
