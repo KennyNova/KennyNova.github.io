3bc644384be0595d84d190395b43765311398fe1
Navid Madani <navidmadani4598@gmail.com>
2021-07-20 04:07:08 -0400

    fixed intro started on about page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import './styles/App.css';
 import './styles/tailwind.css';
-import { splitWord } from './module/split.js';
+import { splitWordAnimation } from './module/split.js';
 
 function App() {
-
-
-
   return (
     <div className="container">
       <div className="intro">
-        {splitWord('hello my name is navid')}
+        <a className="know-more" href="#about">click me</a>
+        {splitWordAnimation('hello my name is navid', "and welcome to my portfolio")}
+        
+      </div>
+      <div id="about" >
+        <h2>about me</h2>
+          <p className="text">
+                      My name is Navid Madani. I am currently a CS student at Georgia State University. I've have always enjoyed learning
+           about new technologies and implementing them into my day to day life. I aspire to become a Full Stack developer as I love
+           working in both the frontend and backend. I am working everyday to achieve this goal by learning as much as I can and then
+           using that knowledge to create apps.
+          </p>
+
+        <h2>projects</h2>
+              <div className="">
+                <a href="https://github.com/KennyNova/React-Weather-App" target="_blank" rel="noopener noreferrer"> 
+                <h2 className="border-b-4 border-blue-400">React Weather App</h2>
+                <div className="">
+                  <p className="">
+                    I created this weather app using react. I went into creating this app with the intention of learning how to access and
+                    implement an api. I learned how to use fetch, to pull information from the api, and learned a lot by styling 
+                    the frontend. I was then able to use this knowledge in other apps.
+                  </p>
+                </div>
+                </a>
+              </div>
       </div>
     </div>
   );
--- src/module/split.js
+++ src/module/split.js
@@ -1,15 +1,32 @@
-export const splitWord = (text) => {
+export const splitWordAnimation = (text1, text2) => {
     
-    const newText = text.split(' ')
+    const newText1 = text1.split(' ')
+    const newText2 = text2.split(' ')
+
 
     return (
-        newText.map((word, index) => (
-            <h1 key={word}
-                className={(index % 2 == 1) ? "tes" : "tes highlight"}
-                style={{
-                    animationDuration: ((index + 2) * .75) + "s"
-                }}
-            >{word}</h1>
-        ))
+        <div className="split">
+            <div>
+            {newText1.map((word, index) => (
+                <h1 key={word}
+                    className={(index % 2 == 1) ? "slide-in-out" : "slide-in-out highlight"}
+                    style={{
+                        animationDuration: ((index + 2) * .75) + "s"
+                    }}
+                >{word}</h1>
+            ))}
+            </div>
+            <div>
+            {newText2.map((word, index) => (
+                <h1 key={word}
+                    className={(index % 2 == 1) ? "slide-in-out2" : "slide-in-out2 highlight"}
+                    style={{
+                        animationDuration: ((index + 2) * .75) + "s"
+                    }}
+                >{word}</h1>
+            ))
+                }
+                </div>
+        </div>
     )
 }
