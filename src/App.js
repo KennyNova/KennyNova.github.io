4a30463157bd5d575792e3de1e4a5ee2ae6fd605
NavidMadani <navidmadani4598@gmail.com>
2021-07-21 19:43:11 -0400

    rework done

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './styles/App.css';
 import './styles/tailwind.css';
 import { splitWordAnimation } from './module/split.js';
 import { Projects } from './components/Projects'
-
-import resume from './other/Navid_M_Resume.docx.pdf';
+import { Contact } from './components/Contact'
+import { Aboutme } from './components/Aboutme'
 
 function App() {
   return (
@@ -11,36 +11,22 @@ function App() {
       <div className="intro">
         <a className="button learn-more top-2/3 md:left-1/3 md:top-1/2" href="#learnMore">learn more</a>
         {splitWordAnimation('hello my name is navid', "and welcome to my portfolio")}
-        
       </div>
+
       <svg className="wave1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"  ><path fill="#142129" fill-opacity="1" d="M0,224L80,229.3C160,235,320,245,480,224C640,203,800,149,960,149.3C1120,149,1280,203,1360,229.3L1440,256L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
+      
       <div id="learnMore" >
-        <div className="about-me">
-          <h2 className="text-9xl md:text-9xl">about me</h2>
-          <a className="button resume float-right" href={resume} target="_blank">resume</a>
-          <p className="text about-desc">
-              My name is Navid Madani. I am currently a CS student at Georgia State University. I've have always enjoyed learning
-            about new technologies and implementing them into my day to day life. I aspire to become a Full Stack developer as I love
-            working in both the frontend and backend. I am working everyday to achieve this goal by learning as much as I can and then
-            using that knowledge to create apps.
-          </p>
-        </div>
-       
+
+        <Aboutme />
        
         <svg className="wave2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"  width="100vw"><path fill="#CAD2C5" fill-opacity="1" d="M0,320L80,272C160,224,320,128,480,128C640,128,800,224,960,250.7C1120,277,1280,235,1360,213.3L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path></svg>
        
-        <Projects/>
-
+        <Projects />
 
         <svg className="wave3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path  fill="#053836" fill-opacity="1" d="M0,64L60,80C120,96,240,128,360,133.3C480,139,600,117,720,96C840,75,960,53,1080,74.7C1200,96,1320,160,1380,192L1440,224L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
 
-          <div className="contact">
-            <h2 className="contact-title text-8xl md:text-9xl text-center">contact</h2>
-            <div className="contact-info">
-              <p>email: navidm4598@gmail.com</p>
-              <a href="https://github.com/KennyNova"><p>github: KennyNova</p></a>
-            </div>
-          </div>
+        <Contact />
+
       </div>
     </div>
   );
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,66 +7,71 @@ export const Projects = (props) => {
       <h2 className="mb-10 text-8xl md:text-9xl float-right">projects</h2>
       
       <div className="project">
-        <a href="https://github.com/KennyNova/React-Weather-App" target="_blank" rel="noopener noreferrer">
-          <img className="project-image float-left w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
-          <div className="float-right">
-              <h3 className="project-title">React Weather App</h3>
-              <div className="project-desc">
-                <p className="text">
-                  I created this weather app using react. I went into creating this app with the intention of learning how to access and
-                  implement an api. I learned how to use fetch, to pull information from the api, and learned a lot by styling 
-                  the frontend. I was then able to use this knowledge in other apps.
-                </p>
-              </div>
-          </div>
-        </a>
+        <img className="project-image float-left w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
+        <div className="float-right">
+            <h3 className="project-title">React Weather App</h3>
+            <div className="project-desc">
+              <p className="text">
+                I created this weather app using react. I went into creating this app with the intention of learning how to access and
+                implement an api. I learned how to use fetch, to pull information from the api, and learned a lot by styling 
+                the frontend. I was then able to use this knowledge in other apps.
+              </p>
+            </div>
+            <div className="project-links">
+              <a className="button live" href="https://react-weather-app-bay-six.vercel.app/" target="_blank" rel="noopener noreferrer">Live</a><a className="button source-code" href="https://github.com/KennyNova/React-Weather-App" target="_blank" rel="noopener noreferrer">Source Code</a>
+            </div>
+        </div>
       </div>
 
       <div className="project ">
-          <a href="https://github.com/KennyNova/Pokedex" target="_blank" rel="noopener noreferrer"> 
-            <img className="project-image float-right w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
-            <div className="float-left">
-              <h3 className="project-title">NextJS Pokedex</h3>
-              <div className="project-desc">
-                  <p className="text">
-                    When creating this Pokedex it was the first time I had ever used NextJS and Tailwind. Tailwind
-                    made styling the frontend much easier and made my workflow much more simpler. I learned how to break down my
-                    code into components and how to use NextJS's way to fetch data from an api.
-                  </p>
-              </div>
-            </div>
-          </a>
+        <img className="project-image float-right w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
+        <div className="float-left">
+          <h3 className="project-title">NextJS Pokedex</h3>
+          <div className="project-desc">
+              <p className="text">
+                When creating this Pokedex it was the first time I had ever used NextJS and Tailwind. Tailwind
+                made styling the frontend much easier and made my workflow much more simpler. I learned how to break down my
+                code into components and how to use NextJS's way to fetch data from an api.
+              </p>
+          </div>
+          <div className="project-links">
+            <a className="button live" href="https://pokedex-ten-beta.vercel.app/" target="_blank" rel="noopener noreferrer">Live</a><a className="button source-code" href="https://github.com/KennyNova/Pokedex" target="_blank" rel="noopener noreferrer">Source Code</a>
+          </div>
+        </div>
       </div>
 
       <div className="project">
-          <a href="https://github.com/KennyNova/messaging-app-socket.io" target="_blank" rel="noopener noreferrer">
-              <img className="project-image float-left w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
-            <div className="float-right">
-              <h3 className="project-title">Socket.io Messaging App</h3>
-              <div className="project-desc">
-                <p className="text">
-                  Creating this chat app was a difficult but very rewarding task. I have learned so much from using Socket.io.
-                  I had also learned how to create my own postgres database and how to connect it to my app to store users and messages.
-                </p>
-              </div>
-            </div>
-          </a>
+        <img className="project-image float-left w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
+        <div className="float-right">
+          <h3 className="project-title">Socket.io Messaging App</h3>
+          <div className="project-desc">
+            <p className="text">
+              Creating this chat app was a difficult but very rewarding task. I have learned so much from using Socket.io.
+              I had also learned how to create my own postgres database and how to connect it to my app to store users and messages.
+            </p>
+          </div>
+          <div className="project-links">
+            <a className="button source-code" href="https://github.com/KennyNova/messaging-app-socket.io" target="_blank" rel="noopener noreferrer">Source Code</a>
+          </div>
         </div>
+      </div>
 
       <div className="last-project">
-        <a href="https://github.com/KennyNova/new-contact-app" target="_blank" rel="noopener noreferrer">
-            <img className="project-image float-right w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
-          <div className="float-left">
-            <h3 className="project-title">Contact App</h3>
-            <div className="project-desc">
-              <p className="text">
-                This contact app was one of my first apps and it helped introduce me to react. I learned about useState and useEffect
-                which are very helpful hooks when creating an app. I also learned how to read and write cookies to save the users contacts.
-              </p>
-            </div>
+        <img className="project-image float-right w-full md:w-4/12" alt="weatherimg" src={weatherApp} />
+        <div className="float-left">
+          <h3 className="project-title">Contact App</h3>
+          <div className="project-desc">
+            <p className="text">
+              This contact app was one of my first apps and it helped introduce me to react. I learned about useState and useEffect
+              which are very helpful hooks when creating an app. I also learned how to read and write cookies to save the users contacts.
+            </p>
           </div>
-        </a>
+          <div className="project-links">
+            <a className="button live" href="https://new-contact-app.vercel.app/" target="_blank" rel="noopener noreferrer">Live</a><a className="button source-code" href="https://github.com/KennyNova/new-contact-app" target="_blank" rel="noopener noreferrer">Source Code</a>
+          </div>
+        </div>
       </div>
+
     </div>
     )
 }
