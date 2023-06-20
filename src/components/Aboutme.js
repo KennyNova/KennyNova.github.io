fe917ce1b02518171799fb9ebd8bac02d68f7d6b
KennyNova <navidmadani4598@gmail.com>
2023-06-20 03:09:02 -0400

    rework on resume display started on hover effect

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -1,12 +1,46 @@
 import '../styles/tailwind.css';
-import resume from '../other/Navid_M_Resume.docx.pdf';
+import resume from '../images/resume.png'
+import {useState} from 'react';
 
 export const Aboutme = (props) => {
+  const [isHovered, setIsHovered] = useState(false);
 
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    console.log("hi")
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
     return (
+
+
         <div className="about-me md:pr-8 md:pl-8">
           <h2 className="text-9xl md:text-9xl">about me</h2>
-          <a className="button resume float-right" href={resume} target="_blank">resume</a>
+          {/* <a className="w-full border-2 md:w-4/5 float-right" >
+          <img className="resume-image float-right w-full md:w-4/12" alt="resumeimg" src={resume} ></img>
+          </a> */}
+
+
+          <div className="relative group">
+            <img
+              src={resume}
+              alt="Image"
+              className="resume-image w-1/2 md:w-4/12"
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+            />
+            {isHovered && (
+              <div className="absolute h-1/2 w-1/2 inset-0 flex items-center justify-center bg-gray-800 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-30 pointer-events-none">
+              <div className="p-2 text-center">
+                Click to Request Resume
+              </div>
+            </div>
+            )}
+          </div>
+
+
           <p className="text about-desc">
               My name is Navid Madani. I am currently a CS student at Georgia State University. I've have always enjoyed learning
             about new technologies and implementing them into my day to day life. I aspire to become a Full Stack developer as I love
